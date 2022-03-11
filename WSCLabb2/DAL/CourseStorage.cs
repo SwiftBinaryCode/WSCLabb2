8b9d3576bf48deb7535479823cad37b5fee063f7
SwiftBinaryCode <89089012+SwiftBinaryCode@users.noreply.github.com>
2022-03-11 13:36:15 +0100

    update

--- WSCLabb2/DAL/CourseStorage.cs
+++ WSCLabb2/DAL/CourseStorage.cs
@@ -1,6 +1,34 @@
-﻿namespace WSCLabb2.DAL
+﻿using WSCLabb2.DAL.Models;
+
+namespace WSCLabb2.DAL
 {
     public class CourseStorage
     {
+
+        private readonly IDictionary<int, Course> _courses;
+
+        private int _id;
+
+        public CourseStorage()
+        {
+
+            _courses = new Dictionary<int, Course>();
+
+        }
+        //Metod som skapar en ny kurs
+
+        //metod som registrerar en användare på en kurs
+
+        //Metod som uppdaterar en kurs
+
+        //Metod som hämtar en list av alla kurser
+
+        //Metod som hämtar en kurs efter specifik id
+
+        //Metod som hämtar en lista av alla anvöndare som är registrerad på den kursen
+
+        //Metod som tar bort en kurs
+
+
     }
 }
--- WSCLabb2/DAL/StudentStorage.cs
+++ WSCLabb2/DAL/StudentStorage.cs
@@ -1,6 +1,73 @@
-﻿namespace WSCLabb2.DAL
+﻿using System.Linq;
+using WSCLabb2.DAL.Models;
+
+namespace WSCLabb2.DAL
 {
     public class StudentStorage
     {
+
+        private readonly IDictionary<int, Student> _students;
+
+        private int _id;
+
+        public StudentStorage()
+        {
+
+            _students = new Dictionary<int, Student>();
+
+        }
+
+        //Metod för att hitta en användare efter deras email
+        public Student? GetUser(int id)
+        {
+            if (!_students.Keys.Contains(id))
+                return null;
+            return _students[id];
+        }
+
+
+        //Metod för att skapa en användare
+        public bool CreateStudent(Student student)
+        {
+            if (_students.Values.Contains(student))
+            {
+                return false;
+            }
+            _students.Add(_id++, student);
+            return true;
+        }
+        //Icollection för att hämta alla användare
+
+        public ICollection<Student> GetAllUsers()
+        {
+            return _students.Values;
+        }
+
+        //metod för att updatera användaren
+        public bool UpdateUser(int id, Student student)
+        {
+            if (!_students.Keys.Contains(id))
+            {
+                return false;
+            }
+
+            _students[id] = student;
+
+            return true;
+        }
+
+        //metod för att ta bort en användare
+
+        public bool DeleteUser(int id)
+        {
+            if (!_students.Keys.Contains(id))
+            {
+                return false;
+            }
+
+            _students.Remove(id);
+
+            return true;
+        }
     }
 }
